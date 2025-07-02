afc0886db7cb61f5dbc20345c4a5c74cca69f12a
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2025-07-02 21:53:05 +0200

    Add boat and sailors

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -1,48 +1,95 @@
 extends Sprite2D
 
-@export var max_speed: float = 320.
+@export var max_speed: float = 500.
 @export var fire_rate: float = 15
 @export var bullet_scene: PackedScene
 @export var bullet_pool_size: int = 5000
+@export var starting_sailor_count: int = 5
 
+@onready var gpu_particles_2d: GPUParticles2D = $GPUParticles2D
+const SAILOR = preload("res://scenes/sailor.tscn")
+var last_direction: Vector2 = Vector2.ZERO
 var bullet_pool: Array[Bullet] = []
 var pool_index: int = 0
 var bullets_to_spawn = 0
 var speed := max_speed
 var initial_scale: Vector2 = Vector2.ONE
 var particle_emitter_orig_pos: Vector2 = Vector2.ZERO
+var boat_lenght:float = 380
 
 func _ready() -> void:
-  particle_emitter_orig_pos = $GPUParticles2D.position
-  for i in bullet_pool_size:
-    bullet_pool.append(bullet_scene.instantiate())
-  initial_scale = bullet_pool[0].scale
+	particle_emitter_orig_pos = gpu_particles_2d.position
+	for i in bullet_pool_size:
+		bullet_pool.append(bullet_scene.instantiate())
+	initial_scale = bullet_pool[0].scale
+	
+	for i in starting_sailor_count:
+		var sailor := SAILOR.instantiate()
+		var offset:Vector2 = Vector2(randf_range(-boat_lenght * 0.5, boat_lenght * 0.5), -35)
+		add_child(sailor)
+		sailor.position = offset
+		sailor.spawn_position = sailor.position
+		sailor.modulate = Color.from_hsv(randf(),
+										 randf_range(0.6,0.8),
+										 randf_range(0.9, 1))
+		
+	
 
 func _process(delta: float) -> void:
-  var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
-  position += direction * speed * delta
+	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+	position += direction * speed * delta
+	
+	if direction != Vector2.ZERO and direction != last_direction:
+		var new_direction: Vector2 = direction - last_direction
+		for child in get_children():
+			if child is Sailor:
+				child.snap_to(-new_direction * speed * delta * Vector2(1,0.5))
+				last_direction = direction
+				child.snap_back()
 
-  if direction.x < 0:
-    $GPUParticles2D.position.x = -particle_emitter_orig_pos.x
-  elif direction.x > 0:
-    $GPUParticles2D.position.x = particle_emitter_orig_pos.x
-  if direction != Vector2.ZERO and !$GPUParticles2D.emitting:
-    $GPUParticles2D.emitting = true
-  elif direction == Vector2.ZERO and $GPUParticles2D.emitting:
-    $GPUParticles2D.emitting = false
+	if direction.x < 0:
+		gpu_particles_2d.position.x = -particle_emitter_orig_pos.x
+	elif direction.x > 0:
+		gpu_particles_2d.position.x = particle_emitter_orig_pos.x
+	if direction != Vector2.ZERO and !gpu_particles_2d.emitting:
+		gpu_particles_2d.emitting = true
+	elif direction == Vector2.ZERO and gpu_particles_2d.emitting:
+		gpu_particles_2d.emitting = false
   
-  if Input.is_action_pressed("fire"):
-    speed = max_speed / 1.75
-    bullets_to_spawn += fire_rate * delta
-    if bullets_to_spawn < 1:
-      pass
-    for i in bullets_to_spawn:
-      var bullet = bullet_pool[pool_index]
-      pool_index = wrapi(pool_index + 1, 0, bullet_pool_size)
-      add_child(bullet)
-      var bullet_transform = global_transform
-      bullet.fire(bullet_transform, 0.03)
-      bullet.scale = initial_scale
-    bullets_to_spawn -= round(bullets_to_spawn)
-  else:
-    speed = max_speed
+	if Input.is_action_pressed("fire"):
+		bullets_to_spawn += fire_rate * delta
+		if bullets_to_spawn < 1:
+			pass
+		for i in bullets_to_spawn:
+			var bullet = bullet_pool[pool_index]
+			pool_index = wrapi(pool_index + 1, 0, bullet_pool_size)
+			add_child(bullet)
+			var bullet_transform = global_transform
+			bullet.fire(bullet_transform, 0.03)
+			bullet.scale = initial_scale
+		bullets_to_spawn -= round(bullets_to_spawn)
+	
+	if Input.is_action_just_pressed("DEBUG_add_sailor"):
+		var sailor := SAILOR.instantiate()
+		var offset:Vector2 = Vector2(randf_range(-boat_lenght * 0.5, boat_lenght * 0.5), -35)
+		add_child(sailor)
+		sailor.spawn_position = offset
+		sailor.position = sailor.spawn_position + Vector2(0,-100)
+		sailor.modulate = Color(randf(),randf(),randf())
+		sailor.spawn()
+	
+	if Input.is_action_just_pressed("DEBUG_remove_sailor"):
+		var sailor = get_random_sailor()
+		if sailor:
+			await sailor.jump_out(direction)
+			sailor.queue_free()
+	
+func get_random_sailor() -> Sailor:
+	var sailors := []
+	for child in get_children():
+		if child is Sailor:
+			if !child.jumping_out:
+				sailors.append(child)
+	if sailors.size() > 0:
+		return sailors[randi() % sailors.size()]
+	return null
---
+++ scripts/sailor.gd
@@ -0,0 +1,61 @@
+class_name Sailor
+extends Sprite2D
+
+@export var delay := 0.2
+@export var snap_duration := 0.25
+@onready var spawn_position: Vector2 = position
+
+var jumping_out: bool = false
+
+func spawn():
+	var current_pos = position
+	await get_tree().create_timer(delay * randf_range(0.1, 0.4)).timeout
+	var tween = create_tween()
+	tween.tween_property(self, "position", spawn_position, snap_duration)\
+		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+	await tween.finished
+
+func snap_to(velocity: Vector2):
+	if jumping_out:
+		return
+	var current_pos = position
+	await get_tree().create_timer(delay * randf_range(0.1, 0.4)).timeout
+	var tween = create_tween()
+	tween.tween_property(self, "position", current_pos + velocity, snap_duration)\
+		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	await tween.finished
+
+func snap_back():
+	if jumping_out:
+		return
+	var current_pos = position
+	await get_tree().create_timer(delay * randf_range(0.1, 0.99)).timeout
+	var tween = create_tween()
+	tween.tween_property(self, "position", spawn_position, snap_duration)\
+		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	await tween.finished
+	
+func jump_out(direction: Vector2):
+	jumping_out = true
+	var verse = Vector2(direction.normalized().x, 1)
+	var start_pos = position
+	var offset = Vector2(randf_range(-200, 0), 90)
+	var jump_position = position + offset * verse
+	var jump_duration = 0.5 + randf_range(0, 0.2)
+	var height = 100  # max height of the arc
+	var random_rotation = deg_to_rad(randf_range(-80, 0) * verse.x)
+	var tween = create_tween()
+	z_index = 2
+	tween.tween_method(
+		func(t):
+			var x = lerp(start_pos.x, jump_position.x, t)
+			var y = lerp(start_pos.y, jump_position.y, t)
+			var arc = -4 * height * (t - 0.5) * (t - 0.5) + height
+			position = Vector2(x, y - arc)
+	,
+	0.0, 1.0, jump_duration
+	).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+
+	tween.parallel().tween_property(self, "rotation", random_rotation, jump_duration)\
+		.set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+	await tween.finished
