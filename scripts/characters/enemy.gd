699bae5131e70297a1b62238533c16e0b7c7a25e
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-07-05 22:11:28 +0200

    Collisions!

--- scripts/characters/enemy.gd
+++ scripts/characters/enemy.gd
@@ -1,15 +1,33 @@
 extends Sprite2D
 
 @export var speed: float = 100
+@export var health_max: int = 100
+
+@onready var health = health_max
+
 signal enemy_spawned
 signal enemy_destroyed
+
+func _ready() -> void:
+	$Label.hide()
 
 func _physics_process(delta: float) -> void:
 	position.x += speed * delta
 
+
 func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
 	enemy_spawned.emit()
+
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	enemy_destroyed.emit()
 	queue_free()
+
+
+func hit(damage: int) -> void:
+	if ($Label.hidden):
+		$Label.show()
+	health -= damage
+	$Label.text = str(health)
+	if (health <= 0):
+		queue_free()
--- scripts/weapons/bullet.gd
+++ scripts/weapons/bullet.gd
@@ -6,21 +6,25 @@ var query := PhysicsShapeQueryParameters2D.new()
 
 @export var max_range: float = 1920
 @export var speed: float = 400
+@export var damage: int = 1
 
 var traveled_distance: float = 0
+
 
 func _physics_process(delta: float) -> void:
 	var distance := speed * delta
 	var motion := transform.x * distance
 	position += motion
 	traveled_distance += distance
-  
+
+
 func _ready() -> void:
 	query.set_shape($Area2D/CollisionShape2D.shape)
 	query.collide_with_bodies = true
 	query.collision_mask = 1
 	top_level = true
-  
+
+
 func _process(delta: float) -> void:
 	var distance := speed * delta
 	var motion := transform.x * distance
@@ -32,9 +36,15 @@ func _process(delta: float) -> void:
 		hide()
 		set_process(false)
 
+
 func fire(new_global_transform: Transform2D,
 	random_rotation: float = 0.0
 ) -> void:
 	global_transform = new_global_transform
 	rotation += randf_range(-random_rotation / 2., random_rotation / 2.)
 	traveled_distance = 0.
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	area.get_parent().call("hit", damage)
+	queue_free()
