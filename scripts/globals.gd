e35b3075052e85b937f6aabfe0c4114e70fb1bb9
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2025-07-06 17:43:56 +0200

    Stuff missing

---
+++ scripts/globals.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var world_speed:float = 500
+
+var current_score:float = 0
+var high_score:float = 0
+
+var tick_score:int = 1 #Score per seconds
+var hit_score:int = 1 #Score per successful hit
+var sink_score:int = 50 #Score per sinked ship
+var sailor_score:int = 10 #Score per collected sailor
+
+var tick_score_multiplier:int = 1 #Multiplier to be added to tick score
+var hit_score_multiplier:int = 1 #Multiplier to be added to hit score
+var sink_score_multiplier:int = 1 #Multiplier to be added to sink score
+var sailor_score_multiplier:int = 1 #Multiplier to be added to sailor score
