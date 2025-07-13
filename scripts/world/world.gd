7671a0e639e04a62a63cd8c88b21b9cb37c85c9a
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2025-07-13 16:06:13 +0200

    New waves

--- scripts/world/world.gd
+++ scripts/world/world.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
 @onready var foreground: Parallax2D = $Foreground
-@onready var sea: Parallax2D = $Sea
+@onready var sea1: Parallax2D = $Sea1
 
 
 func _process(delta):
 	var scrolling_speed = -Globals.world_speed
-	sea.scroll_offset.x += scrolling_speed * delta * 0.7
+	sea1.scroll_offset.x += scrolling_speed * delta * 0.7
 	foreground.scroll_offset.x += scrolling_speed * delta
