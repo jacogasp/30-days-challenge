92216a3950b529d38ee2f70f266b2f234c26d608
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-07-14 08:37:23 +0200

    Reorganize file structure

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,12 @@
 extends Node
 
+@export var game_over_screen_scene: PackedScene
+
 @onready var hud: CanvasLayer = $Hud
 
 func _ready() -> void:
 	GameManager.game_over.connect(_on_game_over)
 
 func _on_game_over() -> void:
-	const GAME_OVER_SCREEN = preload("res://scenes/game_over_screen.tscn")
-	var game_over_screen = GAME_OVER_SCREEN.instantiate()
+	var game_over_screen = game_over_screen_scene.instantiate()
 	hud.add_child(game_over_screen)
